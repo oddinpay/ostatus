c5a353bc80de1c27e96df3c68d0d6c88614b2d2b
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-03-10 17:21:40 +0900

    fix: Include deletion status in SSE handler for missing entries

--- api/main.go
+++ api/main.go
@@ -841,7 +841,7 @@ func Sse(w http.ResponseWriter, r *http.Request) {
 
 			for name, payload := range update {
 
-				idx := lookup[name]
+				idx, found := lookup[name]
 
 				out := map[string]any{
 					"index": idx,
@@ -849,6 +849,10 @@ func Sse(w http.ResponseWriter, r *http.Request) {
 						"probe": payload.Probe,
 						"sla":   payload.SLA,
 					},
+				}
+
+				if !found {
+					out["deleted"] = true
 				}
 
 				if err := conn.SendData(ctx, out); err != nil {
@@ -867,7 +871,7 @@ func sendUpdateToConn(ctx context.Context, conn *sse.Conn, update map[string]Sta
 
 	for name, payload := range update {
 
-		idx := lookup[name]
+		idx, found := lookup[name]
 
 		out := map[string]any{
 			"index": idx,
@@ -875,6 +879,10 @@ func sendUpdateToConn(ctx context.Context, conn *sse.Conn, update map[string]Sta
 				"probe": payload.Probe,
 				"sla":   payload.SLA,
 			},
+		}
+
+		if !found {
+			out["deleted"] = true
 		}
 
 		if err := conn.SendData(ctx, out); err != nil {
