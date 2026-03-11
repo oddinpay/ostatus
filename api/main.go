3bb11bb609a40c791d9a6c1510edeb0bb1c6a1c8
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-03-11 15:16:35 +0900

    fix: Clear probe ID and remove SLA ID from payload in SSE update

--- api/main.go
+++ api/main.go
@@ -929,6 +929,9 @@ func sendUpdateToConn(ctx context.Context, conn *sse.Conn, update map[string]Sta
 			idx = i
 		}
 
+		payload.Probe.Id = ""
+		delete(payload.SLA, "id")
+
 		out := map[string]any{
 			"index":   idx,
 			"payload": map[string]any{"probe": payload.Probe, "sla": payload.SLA},
