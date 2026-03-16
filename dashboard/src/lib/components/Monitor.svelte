000f4539cfc5338c2f12b1654df1056d6565c06e
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-03-17 08:19:51 +0900

    fix: Update button label from "Add monitor" to "Create monitor" for clarity in Monitor component

--- dashboard/src/lib/components/Monitor.svelte
+++ dashboard/src/lib/components/Monitor.svelte
@@ -53,7 +53,7 @@
 		<div class="flex gap-2">
 			<Dialog.Root>
 				<Dialog.Trigger class={cn('cursor-pointer', buttonVariants({ variant: 'outline' }))}
-					>Add monitor</Dialog.Trigger
+					>Create monitor</Dialog.Trigger
 				>
 				<Dialog.Content class="bg-zinc-900">
 					<div class="flex flex-col items-center gap-2">
