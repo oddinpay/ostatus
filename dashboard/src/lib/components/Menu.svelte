a33099bb605e8667f1188a9615dabe429da194c0
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-03-18 05:35:35 +0900

    feat: implement profile image upload and update dialog for favicon settings

--- dashboard/src/lib/components/Menu.svelte
+++ dashboard/src/lib/components/Menu.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import MoreHorizontal from "@lucide/svelte/icons/more-horizontal";
-
   import { Button, buttonVariants } from "$lib/components/ui/button/index.js";
   import * as Dialog from "$lib/components/ui/dialog/index.js";
   import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
@@ -8,9 +7,65 @@
   import { Label } from "$lib/components/ui/label/index.js";
   import { Textarea } from "$lib/components/ui/textarea/index.js";
   import * as Field from "$lib/components/ui/field/index.js";
+  import { superForm } from "sveltekit-superforms";
+  import { zod4 } from "sveltekit-superforms/adapters";
+  import { toast } from "svelte-sonner";
+  import * as Form from "$lib/components/ui/form/index.js";
+  import { formSchema } from "$lib/types/form";
+  import { cn } from "$lib/utils";
+  import { page } from "$app/state";
+  import * as Empty from "$lib/components/ui/empty/index.js";
+  import IconFileOrientation from "@tabler/icons-svelte/icons/file-orientation";
+  import ArrowUpRightIcon from "@lucide/svelte/icons/arrow-up-right";
+  import { useImageUpload } from "$lib/hooks/use-image-upload.svelte";
+  import ImagePlus from "@lucide/svelte/icons/image-plus";
+  import Loader2 from "@lucide/svelte/icons/loader-2";
 
-  let showNewDialog = $state(false);
+  let open = $state(false);
   let showShareDialog = $state(false);
+
+  const profileImageHandler = useImageUpload({
+    initialImage: "",
+  });
+
+  let profileImageBase64: string | null = null;
+
+  const form = superForm(page.data.form, {
+    resetForm: true,
+    validators: zod4(formSchema),
+    onSubmit: async () => {
+      await new Promise((resolve) => setTimeout(resolve, 800));
+    },
+    onUpdate: async ({ form: f }) => {
+      if (f.valid) {
+        open = false;
+        toast.success(`You submitted ${JSON.stringify(f.data, null, 2)}`);
+      } else {
+        open = false;
+        toast.error("Something went wrong. Please try again.");
+      }
+    },
+  });
+
+  const { form: formData, submitting, enhance } = form;
+
+  $effect(() => {
+    if (profileImageHandler.files?.length) {
+      const file = profileImageHandler.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        const result = reader.result;
+        if (typeof result === "string") {
+          $formData.image = result.split(",")[1];
+          profileImageBase64 = result.split(",")[1];
+          console.log("Base64 Image String:", profileImageBase64);
+        }
+      };
+      reader.readAsDataURL(file);
+    } else {
+      profileImageBase64 = null;
+    }
+  });
 </script>
 
 <DropdownMenu.Root>
@@ -22,10 +77,7 @@
   </DropdownMenu.Trigger>
   <DropdownMenu.Content class="w-40" align="end">
     <DropdownMenu.Group>
-      <DropdownMenu.Item
-        class="cursor-pointer"
-        onSelect={() => (showNewDialog = true)}
-      >
+      <DropdownMenu.Item class="cursor-pointer" onSelect={() => (open = true)}>
         Edit
       </DropdownMenu.Item>
       <DropdownMenu.Item
@@ -38,26 +90,137 @@
   </DropdownMenu.Content>
 </DropdownMenu.Root>
 
-<Dialog.Root bind:open={showNewDialog}>
-  <Dialog.Content class="sm:max-w-106.25">
-    <Dialog.Header>
-      <Dialog.Title>Create New File</Dialog.Title>
-      <Dialog.Description>
-        Provide a name for your new file. Click create when you&apos;re done.
-      </Dialog.Description>
-    </Dialog.Header>
-    <Field.Group class="pb-3">
-      <Field.Field>
-        <Field.Label for="filename">File Name</Field.Label>
-        <Input id="filename" name="filename" placeholder="document.txt" />
-      </Field.Field>
-    </Field.Group>
-    <Dialog.Footer>
-      <Dialog.Close class={buttonVariants({ variant: "outline" })}
-        >Cancel</Dialog.Close
+<Dialog.Root bind:open>
+  <Dialog.Content class="bg-zinc-900">
+    <div class="flex flex-col items-center gap-2">
+      <div
+        class="flex size-11 shrink-0 items-center justify-center rounded-full border border-border"
       >
-      <Button type="submit">Create</Button>
-    </Dialog.Footer>
+        {@render Avatar()}
+      </div>
+
+      <Dialog.Header>
+        <Dialog.Title class="mt-10 text-gray-300 sm:text-center"
+          >Favicon</Dialog.Title
+        >
+        <Dialog.Description class="text-gray-400 sm:text-center">
+          Set up and update your status page.
+        </Dialog.Description>
+      </Dialog.Header>
+    </div>
+
+    <form method="POST" class="space-y-5" use:enhance>
+      <div class="space-y-4">
+        <div class="space-y-2">
+          <Form.Field {form} name="title">
+            <Form.Control>
+              {#snippet children({ props })}
+                <Form.Label class="font-bold text-gray-300">Title</Form.Label>
+                <Input
+                  class="border-zinc-700 bg-transparent text-white"
+                  placeholder="Status • Oddin Pay"
+                  type="text"
+                  {...props}
+                  bind:value={$formData.title}
+                />
+              {/snippet}
+            </Form.Control>
+            <Form.FieldErrors />
+          </Form.Field>
+        </div>
+
+        <div class="space-y-2">
+          <Form.Field {form} name="description">
+            <Form.Control>
+              {#snippet children({ props })}
+                <Form.Label class="font-bold text-gray-300"
+                  >Description</Form.Label
+                >
+                <Input
+                  class="border-zinc-700 bg-transparent text-white"
+                  placeholder="OddinPay system performance."
+                  type="text"
+                  {...props}
+                  bind:value={$formData.description}
+                />
+              {/snippet}
+            </Form.Control>
+            <Form.FieldErrors />
+          </Form.Field>
+        </div>
+
+        <div class="space-y-2">
+          <Form.Field {form} name="navbar">
+            <Form.Control>
+              {#snippet children({ props })}
+                <Form.Label class="font-bold text-gray-300"
+                  >Text Logo</Form.Label
+                >
+                <Input
+                  class="border-zinc-700 bg-transparent text-white"
+                  placeholder="Oddin Status"
+                  type="text"
+                  {...props}
+                  bind:value={$formData.navbar}
+                />
+              {/snippet}
+            </Form.Control>
+            <Form.FieldErrors />
+          </Form.Field>
+        </div>
+
+        <div class="space-y-2">
+          <Form.Field {form} name="signup">
+            <Form.Control>
+              {#snippet children({ props })}
+                <Form.Label class="font-bold text-gray-300"
+                  >Signup URL</Form.Label
+                >
+                <Input
+                  class="border-zinc-700 bg-transparent text-white"
+                  placeholder="https://odinpay.com/signup"
+                  type="text"
+                  {...props}
+                  bind:value={$formData.signup}
+                />
+              {/snippet}
+            </Form.Control>
+            <Form.FieldErrors />
+          </Form.Field>
+        </div>
+
+        <div class="space-y-2">
+          <Form.Field {form} name="signin">
+            <Form.Control>
+              {#snippet children({ props })}
+                <Form.Label class="font-bold text-gray-300"
+                  >Signin URL</Form.Label
+                >
+                <Input
+                  class="border-zinc-700 bg-transparent text-white"
+                  placeholder="https://odinpay.com/signin"
+                  type="text"
+                  {...props}
+                  bind:value={$formData.signin}
+                />
+              {/snippet}
+            </Form.Control>
+            <Form.FieldErrors />
+          </Form.Field>
+        </div>
+      </div>
+      <Form.Button
+        class="mt-2 w-full cursor-pointer "
+        type="submit"
+        variant="outline"
+        disabled={$submitting}
+        >{#if $submitting}
+          <Loader2 class="size-4 animate-spin" />
+        {:else}
+          Update 
+        {/if}
+      </Form.Button>
+    </form>
   </Dialog.Content>
 </Dialog.Root>
 
@@ -96,3 +259,39 @@
     </Dialog.Footer>
   </Dialog.Content>
 </Dialog.Root>
+
+{#snippet Avatar()}
+  <label class="mt-10 cursor-pointer px-6" aria-label="Upload profile picture">
+    <div
+      class="relative flex size-20 items-center justify-center overflow-hidden rounded-full border-4 border-zinc-600 bg-zinc-700 shadow-xs shadow-black/10"
+    >
+      {#if profileImageHandler.previewUrl}
+        <img
+          src={profileImageHandler.previewUrl}
+          class="size-full object-cover"
+          width={80}
+          height={80}
+          alt="Profile avatar"
+        />
+      {/if}
+      <button
+        type="button"
+        class="absolute inset-0 flex cursor-pointer items-center justify-center rounded-full bg-zinc-800 text-white transition-[color,box-shadow,background-color,backdrop-filter] outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 {profileImageHandler.previewUrl
+          ? 'hidden hover:flex'
+          : 'flex'} hover:bg-black/50"
+        onclick={profileImageHandler.handleThumbnailClick}
+        aria-label="Change profile picture"
+      >
+        <ImagePlus size={16} aria-hidden="true" />
+      </button>
+      <input
+        type="file"
+        bind:this={profileImageHandler.fileInput}
+        bind:files={profileImageHandler.files}
+        class="hidden"
+        accept="image/*"
+        aria-label="Upload profile picture"
+      />
+    </div>
+  </label>
+{/snippet}
--- dashboard/src/routes/+layout.svelte
+++ dashboard/src/routes/+layout.svelte
@@ -1,35 +1,45 @@
 <script lang="ts">
-	import '../app.css';
-	import '@fontsource-variable/inter';
-	import { PUBLIC_CONVEX_URL } from '$env/static/public';
-	import { setupConvex } from 'convex-svelte';
-	let { children } = $props();
-	setupConvex(PUBLIC_CONVEX_URL);
+  import "../app.css";
+  import "@fontsource-variable/inter";
+  import { PUBLIC_CONVEX_URL } from "$env/static/public";
+  import { setupConvex } from "convex-svelte";
+  let { children } = $props();
+  setupConvex(PUBLIC_CONVEX_URL);
+  import { Toaster } from "svelte-sonner";
 
-	let base64 = "";
+  let base64 = "";
 
-	function detectImageTypeFromBase64(base64:string): string {
-  
-		const firstChar = base64.trim();
-		switch (firstChar) {
- 		  	 case '/': return 'image/jpeg';   // JPEG
-  		 	 case 'i': return 'image/png';    // PNG
-   			 case 'A': return 'image/ico'     // ICO
-    		 case 'P': return 'image/svg+xml';// SVG
-			 case 'U': return 'image/webp';   // WEBP
-			 case 'T': return 'image/tiff';   // TIFF
-			 case 'Q': return 'image/bmp';    // BMP
-			 case 'AAAA': return 'image/avif';    // AVIF
-			 default : return '';     // Default fallback
-		}
-	}
+  function detectImageTypeFromBase64(base64: string): string {
+    const firstChar = base64.trim();
+    switch (firstChar) {
+      case "/":
+        return "image/jpeg"; // JPEG
+      case "i":
+        return "image/png"; // PNG
+      case "A":
+        return "image/ico"; // ICO
+      case "P":
+        return "image/svg+xml"; // SVG
+      case "U":
+        return "image/webp"; // WEBP
+      case "T":
+        return "image/tiff"; // TIFF
+      case "Q":
+        return "image/bmp"; // BMP
+      case "AAAA":
+        return "image/avif"; // AVIF
+      default:
+        return ""; // Default fallback
+    }
+  }
 
-	let faviconUrl = `data:${detectImageTypeFromBase64(base64)};base64,${base64}`;
-
+  let faviconUrl = `data:${detectImageTypeFromBase64(base64)};base64,${base64}`;
 </script>
 
 <svelte:head>
-	<link rel="icon" href="{faviconUrl}" />
+  <link rel="icon" href={faviconUrl} />
 </svelte:head>
+
+<Toaster closeButton position="top-center" />
 
 {@render children?.()}
