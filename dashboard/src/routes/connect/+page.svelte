234ae7dff6ce1d003dc820d4f431a6004b0208fe
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-03-18 07:15:26 +0900

    fix: update layout and styling of connect page for improved responsiveness

--- dashboard/src/routes/connect/+page.svelte
+++ dashboard/src/routes/connect/+page.svelte
@@ -1,49 +1,58 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	// import Connect from '$lib/components/Connect.svelte';
-	import Header from '$lib/components/Header.svelte';
-	import { Tabs, TabsContent, TabsList, TabsTrigger } from '$lib/components/ui/tabs';
-	import { Unplug, House } from 'lucide-svelte';
+  import { goto } from "$app/navigation";
+  // import Connect from '$lib/components/Connect.svelte';
+  import Header from "$lib/components/Header.svelte";
+  import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+  } from "$lib/components/ui/tabs";
+  import { Unplug, House } from "lucide-svelte";
 
-	let currentTab = 'tab-5';
+  let currentTab = "tab-5";
 </script>
 
-<div class="flex min-h-screen items-start justify-center overflow-hidden bg-black">
-	<div class="fixed mx-auto w-full max-w-7xl py-4">
-		<div class="px-4 py-4">
-			<Header />
-		</div>
-		<Tabs
-			value={currentTab}
-			class="flex max-h-[calc(100dvh-3rem)] w-full flex-col gap-8 md:flex-row"
-		>
-			<TabsList
-				class="sticky top-6 flex w-80 flex-col gap-2 self-start bg-transparent p-4 max-[770px]:top-0 max-[770px]:z-50 max-[770px]:w-full max-[770px]:flex-row max-[770px]:justify-around max-[770px]:bg-black/80 max-[770px]:backdrop-blur-md"
-			>
-				<TabsTrigger
-					value="tab-0"
-					onclick={() => goto('/')}
-					class="flex w-full cursor-pointer items-center justify-start gap-2 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[770px]:justify-center max-[770px]:p-2 xl:px-6 xl:text-3xl"
-				>
-					<House class="h-8 w-8" />
-					<span class="max-[400px]:hidden">Home</span>
-				</TabsTrigger>
+<div
+  class="flex min-h-screen items-start justify-center overflow-hidden bg-black"
+>
+  <div class="fixed mx-auto w-full max-w-7xl py-4">
+    <div class="px-4 py-4">
+      <Header />
+    </div>
+    <Tabs
+      value={currentTab}
+      class="flex max-h-[calc(100dvh-3rem)] w-full flex-col gap-8 900:flex-row"
+    >
+      <TabsList
+        class="sticky top-6 flex w-80 flex-col gap-2 self-start bg-transparent p-4 max-[900px]:top-0 max-[900px]:z-50 max-[900px]:w-full max-[900px]:flex-row max-[900px]:justify-around max-[900px]:bg-black/80 max-[900px]:backdrop-blur-md"
+      >
+        <TabsTrigger
+          value="tab-0"
+          onclick={() => goto("/")}
+          class="flex w-full cursor-pointer items-center justify-start gap-2 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[900px]:justify-center max-[900px]:p-2 xl:px-6 xl:text-3xl"
+        >
+          <House class="h-8 w-8" />
+          <span class="max-[400px]:hidden">Home</span>
+        </TabsTrigger>
 
-				<TabsTrigger
-					value="tab-5"
-					onclick={() => goto('/connect')}
-					class="flex w-full cursor-pointer items-center justify-start gap-2 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[770px]:justify-center max-[770px]:p-2 xl:px-6 xl:text-3xl"
-				>
-					<Unplug class="h-8 w-8" />
-					<span class="max-[400px]:hidden">Connect</span>
-				</TabsTrigger>
-			</TabsList>
+        <TabsTrigger
+          value="tab-5"
+          onclick={() => goto("/connect")}
+          class="flex w-full cursor-pointer items-center justify-start gap-2 rounded-md py-4 text-2xl font-bold text-white transition data-[state=active]:bg-transparent data-[state=active]:text-white data-[state=inactive]:text-zinc-400 data-[state=inactive]:hover:bg-zinc-800 data-[state=inactive]:hover:text-white max-[900px]:justify-center max-[900px]:p-2 xl:px-6 xl:text-3xl"
+        >
+          <Unplug class="h-8 w-8" />
+          <span class="max-[400px]:hidden">Connect</span>
+        </TabsTrigger>
+      </TabsList>
 
-			<div class="grid w-full grid-cols-1 gap-4 overflow-y-auto px-10 pb-20 md:grid-cols-2">
-				<div
-					class="relative col-span-1 rounded-lg border border-border bg-zinc-900 p-8 md:col-span-2 md:min-h-[480px] md:overflow-y-hidden"
-				>
-					<!-- <TabsContent value="tab-5" class="h-auto min-h-[300px]">
+      <div
+        class="grid w-full grid-cols-1 gap-4 overflow-y-auto px-10 pb-20 900:grid-cols-2"
+      >
+        <div
+          class="relative col-span-1 rounded-lg border border-border bg-zinc-900 p-8 900:col-span-2 900:min-h-120 900:overflow-y-hidden"
+        >
+          <!-- <TabsContent value="tab-5" class="h-auto min-h-[300px]">
 						<Tabs value="tab-5" class=" items-center">
 							<TabsList
 								class="h-auto gap-2 rounded-full border-b border-border bg-zinc-800 px-10 py-2 text-zinc-400"
@@ -69,8 +78,8 @@
 							</TabsContent>
 						</Tabs>
 					</TabsContent> -->
-				</div>
-			</div>
-		</Tabs>
-	</div>
+        </div>
+      </div>
+    </Tabs>
+  </div>
 </div>
