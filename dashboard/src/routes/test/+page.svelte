a7d0248e45b72e23b1afcdc32d1db3bbcdf93bad
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-03-19 01:33:52 +0900

    refactor: format code for consistency and improve readability in status query component

--- dashboard/src/routes/test/+page.svelte
+++ dashboard/src/routes/test/+page.svelte
@@ -1,33 +1,30 @@
 <script lang="ts">
-	import { useQuery } from 'convex-svelte';
-	import { api } from '../../convex/_generated/api';
-	import { env } from '$env/dynamic/public';
-	
-	const query = useQuery(api.status.get, {apiKey: env.PUBLIC_API_KEY});
+  import { useQuery } from "convex-svelte";
+  import { api } from "../../convex/_generated/api";
+  import { env } from "$env/dynamic/public";
+
+  const query = useQuery(api.status.get, { apiKey: env.PUBLIC_API_KEY });
 </script>
 
 {#if query.isLoading}
-	Loading...
+  Loading...
 {:else if query.error}
-	failed to load: {query.error.toString()}
+  failed to load: {query.error.toString()}
 {:else}
-	<ul>
-		{#each query.data as status}
-			<li>
-				<span>Assigner: {status.assigner}</span>
-			</li>
-			<li>
-				<span>Protocol: {status.protocol}</span>
-			</li>
-			<li>
-				<span>Name: {status.name}</span>
-			</li>
-			<li>
-				<span>Host: {status.host}</span>
-			</li>
-			<li>
-				<span>Interval: {status.interval}</span>
-			</li>
-		{/each}
-	</ul>
+  <ul>
+    {#each query.data as status}
+      <li>
+        <span>Protocol: {status.protocol}</span>
+      </li>
+      <li>
+        <span>Name: {status.name}</span>
+      </li>
+      <li>
+        <span>Host: {status.host}</span>
+      </li>
+      <li>
+        <span>Interval: {status.interval}</span>
+      </li>
+    {/each}
+  </ul>
 {/if}
