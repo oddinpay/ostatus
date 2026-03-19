085f02dc3a45a6784d23e3b7716fc2b08d02cb9f
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-03-19 10:48:36 +0900

    fix: improve error handling and return value in post mutation for status

--- dashboard/src/convex/status.ts
+++ dashboard/src/convex/status.ts
@@ -29,16 +29,17 @@ export const post = mutation({
     protocol: v.string(),
   },
   handler: async (ctx, args) => {
-    if (args.apiKey !== process.env.API_KEY) throw new Error("Unauthorized");
-
-    const monitorId = await ctx.db.insert("status", {
+    if (args.apiKey !== process.env.API_KEY) {
+      throw new Error("Unauthorized");
+    }
+    const monitor = await ctx.db.insert("status", {
       host: args.host,
       interval: args.interval,
       name: args.name,
       protocol: args.protocol,
     });
 
-    return monitorId;
+    return monitor;
   },
 });
 
