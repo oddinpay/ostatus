79e62d9039867292387f0e3c8648f0e5fcd59ea8
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-03-30 18:32:24 +0900

    fix: enhance form structure and add monitor type selection in data table actions component

--- dashboard/src/lib/components/ui/data-table/data-table-actions.svelte
+++ dashboard/src/lib/components/ui/data-table/data-table-actions.svelte
@@ -12,6 +12,7 @@
     confirmDelete,
   } from "$lib/components/ui/confirm-delete-dialog";
 
+  import * as Select from "$lib/components/ui/select/index.js";
   import * as Form from "$lib/components/ui/form/index.js";
   import Loader2 from "@lucide/svelte/icons/loader-2";
   import { Input } from "$lib/components/ui/input/index.js";
@@ -21,6 +22,13 @@
   let { id }: { id: string } = $props();
 
   let open = $state(false);
+
+  const services = [
+    { value: "https", label: "HTTPS" },
+    { value: "http", label: "HTTP" },
+    { value: "tcp", label: "TCP" },
+    { value: "dns", label: "DNS" },
+  ];
 
   const form = superForm(page.data.form, {
     id: "update-monitor",
@@ -41,10 +49,20 @@
   });
 
   const { form: formData, submitting, enhance } = form;
+
+  const triggerContent = $derived(
+    services.find((f) => f.value === $formData.monitorType)?.label ?? "HTTPS",
+  );
+
+  $effect(() => {
+    if (!$formData.monitorType) {
+      $formData.monitorType = "https";
+    }
+  });
 </script>
 
 <Dialog.Root bind:open>
-  <Dialog.Content class="bg-zinc-900">
+  <Dialog.Content class="bg-zinc-900 ">
     <div class="flex flex-col items-center gap-2">
       <div
         class="flex size-10 shrink-0 items-center justify-center rounded-full border border-border"
@@ -81,39 +99,20 @@
             </Form.Control>
             <Form.FieldErrors />
           </Form.Field>
-        </div>
 
-        <div class="space-y-2">
-          <Form.Field {form} name="title">
+          <Form.Field {form} name="name">
             <Form.Control>
               {#snippet children({ props })}
-                <Form.Label class="font-bold text-gray-300">Title</Form.Label>
-                <Input
-                  class="border-zinc-700 bg-transparent text-white"
-                  placeholder="Status • Oddin Pay"
-                  type="text"
-                  {...props}
-                  bind:value={$formData.title}
-                />
-              {/snippet}
-            </Form.Control>
-            <Form.FieldErrors />
-          </Form.Field>
-        </div>
-
-        <div class="space-y-2">
-          <Form.Field {form} name="description">
-            <Form.Control>
-              {#snippet children({ props })}
-                <Form.Label class="font-bold text-gray-300"
-                  >Description</Form.Label
+                <Form.Label class="font-bold text-gray-300" for="logo"
+                  >Name</Form.Label
                 >
                 <Input
-                  class="border-zinc-700 bg-transparent text-white"
-                  placeholder="OddinPay system performance."
+                  class=" border-zinc-700 bg-transparent text-white"
+                  placeholder="oddinpay"
                   type="text"
                   {...props}
-                  bind:value={$formData.description}
+                  bind:value={$formData.name}
+                  required
                 />
               {/snippet}
             </Form.Control>
@@ -121,98 +120,97 @@
           </Form.Field>
         </div>
 
-        <div class="space-y-2">
-          <Form.Field {form} name="slug">
-            <Form.Control>
-              {#snippet children({ props })}
-                <Form.Label class="font-bold text-gray-300">Slug</Form.Label>
-                <Input
-                  class="border-zinc-700 bg-transparent text-white"
-                  placeholder="https://oddinpay.com"
-                  type="text"
-                  {...props}
-                  bind:value={$formData.slug}
-                />
-              {/snippet}
-            </Form.Control>
-            <Form.FieldErrors />
-          </Form.Field>
-        </div>
+        <Form.Field {form} name="protocol">
+          <Form.Control>
+            {#snippet children({ props })}
+              <Form.Label class="font-bold text-gray-300"
+                >Monitor Type</Form.Label
+              >
+              <Select.Root
+                type="single"
+                bind:value={$formData.monitorType}
+                name={props.name}
+              >
+                <Select.Trigger
+                  class="w-full cursor-pointer border-zinc-700 text-white"
+                >
+                  {triggerContent}
+                </Select.Trigger>
 
-        <div class="space-y-2">
-          <Form.Field {form} name="textLogo">
+                <Select.Content class="bg-zinc-800 border-zinc-700 text-white">
+                  {#each services as type}
+                    <Select.Item
+                      class="cursor-pointer data-highlighted:bg-zinc-700 data-highlighted:text-white [&_svg:not([class*='text-'])]:text-gray-500"
+                      value={type.value}
+                      label={type.label}
+                    >
+                      {type.label}
+                    </Select.Item>
+                  {/each}
+                </Select.Content>
+              </Select.Root>
+            {/snippet}
+          </Form.Control>
+          <Form.FieldErrors />
+        </Form.Field>
+
+        <Form.Field {form} name="host">
+          <Form.Control>
+            {#snippet children({ props })}
+              <Form.Label class="font-bold text-gray-300">
+                {$formData.monitorType === "dns" ||
+                $formData.monitorType === "tcp"
+                  ? "Host"
+                  : "Domain"}
+              </Form.Label>
+              <Input
+                {...props}
+                class="border-zinc-700 bg-transparent text-white"
+                placeholder={$formData.monitorType === "tcp"
+                  ? "127.0.0.1:8080"
+                  : "www.oddinpay.com"}
+                bind:value={$formData.url}
+              />
+            {/snippet}
+          </Form.Control>
+          <Form.FieldErrors />
+        </Form.Field>
+
+        <div class="space-y-4">
+          <Form.Field {form} name="interval">
             <Form.Control>
               {#snippet children({ props })}
-                <Form.Label class="font-bold text-gray-300"
-                  >Text Logo</Form.Label
+                <Form.Label class="font-bold text-gray-300" for="interval"
+                  >Interval</Form.Label
                 >
                 <Input
-                  class="border-zinc-700 bg-transparent text-white"
-                  placeholder="Oddin Status"
-                  type="text"
+                  class=" border-zinc-700 bg-transparent text-white"
+                  placeholder="10s"
+                  type="number"
                   {...props}
-                  bind:value={$formData.textLogo}
+                  bind:value={$formData.interval}
+                  required
                 />
               {/snippet}
             </Form.Control>
             <Form.FieldErrors />
           </Form.Field>
         </div>
-
-        <div class="space-y-2">
-          <Form.Field {form} name="signup">
-            <Form.Control>
-              {#snippet children({ props })}
-                <Form.Label class="font-bold text-gray-300"
-                  >Signup URL</Form.Label
-                >
-                <Input
-                  class="border-zinc-700 bg-transparent text-white"
-                  placeholder="https://odinpay.com/signup"
-                  type="text"
-                  {...props}
-                  bind:value={$formData.signup}
-                />
-              {/snippet}
-            </Form.Control>
-            <Form.FieldErrors />
-          </Form.Field>
-        </div>
-
-        <div class="space-y-2">
-          <Form.Field {form} name="signin">
-            <Form.Control>
-              {#snippet children({ props })}
-                <Form.Label class="font-bold text-gray-300"
-                  >Signin URL</Form.Label
-                >
-                <Input
-                  class="border-zinc-700 bg-transparent text-white"
-                  placeholder="https://odinpay.com/signin"
-                  type="text"
-                  {...props}
-                  bind:value={$formData.signin}
-                />
-              {/snippet}
-            </Form.Control>
-            <Form.FieldErrors />
-          </Form.Field>
-        </div>
+        <Form.Button
+          formaction="?/update"
+          class="mt-2 w-full cursor-pointer disabled:pointer-events-auto disabled:cursor-not-allowed"
+          type="submit"
+          variant="outline"
+          disabled={$submitting}
+          >{#if $submitting}
+            <Loader2 class="size-4 animate-spin" />
+          {:else}
+            Save
+          {/if}
+        </Form.Button>
       </div>
-      <Form.Button
-        formaction="?/update"
-        class="mt-2 w-full cursor-pointer disabled:pointer-events-auto disabled:cursor-not-allowed"
-        type="submit"
-        variant="outline"
-        disabled={$submitting}
-        >{#if $submitting}
-          <Loader2 class="size-4 animate-spin" />
-        {:else}
-          Update
-        {/if}
-      </Form.Button>
-    </form>
-  </Dialog.Content>
+    </form></Dialog.Content
+  >
 </Dialog.Root>
 
 <DropdownMenu.Root>
