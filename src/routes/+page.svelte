376b565065112c94dbabd10444da51c3be76c7e3
0x64 <127573781+sachinsenal0x64@users.noreply.github.com>
2026-04-27 04:25:24 +0800

    fix: enhance maintenance list filtering to include 'Scheduled' status checks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1398,7 +1398,7 @@
                         </div>
                       </div>
                     {:else if i === 1}
-                      {#if (incidents.length === 0 && maintenances.length === 0) || (!incidents.some( (i) => i.entries.some((e) => e.status === Indicators.Resolved), ) && !maintenances.some( (m) => m.entries.some((e) => e.status === Indicators.Completed || e.status === Indicators.Cancelled || e.status === Indicators.Scheduled), ))}
+                      {#if (incidents.length === 0 && maintenances.length === 0) || !!maintenances.some((m) => m.entries.some((e) => e.status === Indicators.Scheduled) || (!incidents.some( (i) => i.entries.some((e) => e.status === Indicators.Resolved), ) && !maintenances.some( (m) => m.entries.some((e) => e.status === Indicators.Completed || e.status === Indicators.Cancelled), )))}
                         <p
                           class="p-10 text-black text-center"
                           style="font-size: 16px"
